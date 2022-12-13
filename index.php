1430e3c923f55435bdd630c54f3a9c95ef4288b5
Eduardo Klein Diefenthaler <120498575+eduardo-klein-dev@users.noreply.github.com>
2022-12-13 14:20:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/icon.webp">
+    <title>Starbucks</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="line"></div>
+        <div class="container header-area">
+            <div class="header-logo">
+                <a href="/"><img src="img/logo.png" alt="Logo Starbucks"></a>
+            </div>
+            <div class="header-conteudo">
+                <div class="header-conteudo-search">
+                    <form>
+                        <input type="text" name="search" placeholder="Search" />
+                        <button> <img src="img/loupe.png" alt="Lupa Pesquisa"></button>
+                    </form>
+                </div>
+                <div class="header-conteudo-menu">
+                    <img src="img/menu.png" alt="Menu">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="container">
+            <h1>Starbucks<br>Happy Hour</h1>
+            <span>We'll meet you in inbox. Drink deals are on their way.</span>
+            <div class="banner-imagens">
+                <img src="img/c1.jpg" alt="Imagen Banner 1">
+                <img src="img/c2.jpg" alt="Imagen Banner 2">
+                <img src="img/c3.jpg" alt="Imagen Banner 3">
+            </div>
+        </div>
+    </section>
+
+    <section class="area1">
+        <div class="area1--content">
+            <div class="bar"></div>
+            <h2>Four Drinks, A Million<br />Reasons</h2>
+            <div class="area1-content-subtitulo"><em>"Just love yourself and you're set." - Lady Garga</em></div>
+            <div class="area1-content-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas cumque impedit architecto perspiciatis error nisi obcaecati ullam, ut neque qui ea laboriosam voluptate libero assumenda blanditiis esse. Fugiat corporis expedita laborum aperiam quo eum, sint nihil id illum sapiente unde beatae dolorem quos ipsam nobis doloremque, omnis a, ab sequi!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas cumque impedit architecto perspiciatis error nisi obcaecati ullam, ut neque qui ea laboriosam voluptate libero assumenda blanditiis esse. Fugiat corporis expedita laborum aperiam quo eum, sint nihil id illum sapiente unde beatae dolorem quos ipsam nobis doloremque, omnis a, ab sequi!</div>
+            <a class="button" href="">learn more</a>
+        </div>
+        <div class="area1--img1"></div>
+        <div class="area1--img2"></div>
+    </section>
+
+    <section class="area2">
+        <div class="area2--content">
+            <div class="bar"></div>
+            <h2>Introducing vanilla Sweet<br>Cream Cold Brew-Sweet<br>Creamy, Cold, Bold.</h2>
+            <div class="area2-content-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas cumque impedit architecto perspiciatis error nisi obcaecati ullam, ut neque qui ea laboriosam voluptate libero assumenda blanditiis esse. Fugiat corporis expedita laborum aperiam quo eum, sint nihil id illum sapiente unde beatae dolorem quos ipsam nobis doloremque, omnis a, ab sequi!</div>
+            <a class="button" href="">learn more</a>
+        </div>
+        <div class="area2--img1"></div>
+        <div class="area2--img2"></div>
+    </section>
+
+    <section class="area3">
+        <div class="area3--content">
+            <h2>The New Ultra Caramel<br>Frappuccino Is Here!</h2>
+            <div class="area3-content-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae cum sequi at laboriosam quisquam, doloribus ipsa ad temporibus quis perspiciatis! Impedit exercitationem.</div>
+            <a class="button" href="">learn more</a>
+        </div>
+        <div class="area3--img1"></div>
+    </section>
+
+    <section class="area4">
+        <div class="area4--content">
+            <div class="bar"></div>
+            <h2>Kindness Starts<br>With A Cup</h2>
+            <div class="area4-content-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae cum sequi at laboriosam quisquam, doloribus ipsa ad temporibus quis perspiciatis! Impedit exercitationem.</div>
+            <a class="button" href="">learn more</a>
+        </div>
+        <div class="area4--img1"></div>
+    </section>
+
+    <section class="area5"></section>
+
+    <footer>
+        Copyright - Todos os direitos reservados<br/>
+        Créditos do layout para Nicepage
+    </footer>
+
+</body>
+
+</html>
